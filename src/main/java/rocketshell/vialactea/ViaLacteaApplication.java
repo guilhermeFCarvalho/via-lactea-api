8fb944e5e581fa0640efd6a61ad433bbd8744105
Willian Gabriel Kuviatkovski de Melo <wkuviatkovski@gmail.com>
2022-09-01 19:47:31 -0300

    Primeiro Commit

---
+++ src/main/java/rocketshell/vialactea/ViaLacteaApplication.java
@@ -0,0 +1,13 @@
+package rocketshell.vialactea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViaLacteaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViaLacteaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rocketshell/vialactea/ViaLacteaApplicationTests.java
@@ -0,0 +1,13 @@
+package rocketshell.vialactea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViaLacteaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
