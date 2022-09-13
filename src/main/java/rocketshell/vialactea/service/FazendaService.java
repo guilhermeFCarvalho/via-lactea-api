a8fe3b5f5fbe3c5b2e0228c10d51920585defaae
julio.treichel <julio.tre@outlook.com.br>
2022-09-13 19:45:51 -0300

    adicionando RequestMapping e Service

--- src/main/java/rocketshell/vialactea/service/FazendaService.java
+++ src/main/java/rocketshell/vialactea/service/FazendaService.java
@@ -1,9 +1,12 @@
 package rocketshell.vialactea.service;
 
+import org.springframework.stereotype.Service;
+
 import rocketshell.vialactea.base.BaseService;
 import rocketshell.vialactea.domain.Fazenda;
 import rocketshell.vialactea.repository.FazendaRepository;
 
+@Service
 public class FazendaService extends BaseService<Fazenda,FazendaRepository> {
   
 }
