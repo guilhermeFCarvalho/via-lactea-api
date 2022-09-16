1bb80f97f082396f9721a16d626a310a82658b94
julio.treichel <julio.tre@outlook.com.br>
2022-09-15 21:43:35 -0300

    #17273 hotfix: adicionando notação RestController

--- src/main/java/rocketshell/vialactea/controller/FazendaController.java
+++ src/main/java/rocketshell/vialactea/controller/FazendaController.java
@@ -2,14 +2,15 @@ package rocketshell.vialactea.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import rocketshell.vialactea.base.BaseController;
 import rocketshell.vialactea.domain.Fazenda;
 import rocketshell.vialactea.repository.FazendaRepository;
 import rocketshell.vialactea.service.FazendaService;
 
-
 @Controller
+@RestController
 @RequestMapping("/fazendas")
 public class FazendaController extends BaseController<Fazenda,FazendaRepository,FazendaService> {
   
