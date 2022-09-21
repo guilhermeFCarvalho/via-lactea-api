006cd643cbde1f22346923b086457036bc757c0e
Rodrigobueno2 <73552177+Rodrigobueno2@users.noreply.github.com>
2022-09-21 14:33:40 -0300

    Adicicionei as importações de Page e Pageable dos metodos GET paginados

--- src/main/java/rocketshell/vialactea/base/BaseController.java
+++ src/main/java/rocketshell/vialactea/base/BaseController.java
@@ -2,6 +2,8 @@ package rocketshell.vialactea.base;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.*;
 
--- src/main/java/rocketshell/vialactea/base/BaseService.java
+++ src/main/java/rocketshell/vialactea/base/BaseService.java
@@ -3,6 +3,8 @@ package rocketshell.vialactea.base;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
