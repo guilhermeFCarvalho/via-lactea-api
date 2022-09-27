f5cf07a8133f5e773ff1ccd69b94e29959e6304c
guilhermeFCarvalho <gui.carvalho021@gmail.com>
2022-09-27 20:32:29 -0300

    adicionados testes de request sem autenticação

---
+++ src/test/java/rocketshell/vialactea/controller/PropriedadesControllerTest.java
@@ -0,0 +1,32 @@
+package rocketshell.vialactea.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.test.web.servlet.MockMvc;
+
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PropriedadesControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testRequestSemAutenticacao() throws Exception {
+
+        RequestBuilder request = MockMvcRequestBuilders.get("/propriedades");
+
+        mockMvc.perform(request).andExpect(status().is4xxClientError());
+
+    }
+
+}
