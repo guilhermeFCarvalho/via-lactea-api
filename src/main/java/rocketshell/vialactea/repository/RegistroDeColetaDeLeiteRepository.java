4c5ec77dcd6abe2d0f3f2b4fc9254b2eac50908a
rodrigo <rodrigomarinelo2@gmail.com>
2022-10-31 20:25:43 -0300

    Fazendo o domain, service, repository e controller da entidade registro de coleta de leite

---
+++ src/main/java/rocketshell/vialactea/repository/RegistroDeColetaDeLeiteRepository.java
@@ -0,0 +1,11 @@
+package rocketshell.vialactea.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import rocketshell.vialactea.domain.RegistroDeColetaDeLeite;
+
+
+
+public interface RegistroDeColetaDeLeiteRepository extends JpaRepository<RegistroDeColetaDeLeite, Long>{
+
+}
---
+++ src/main/java/rocketshell/vialactea/service/RegistroDeColetaDeLeiteService.java
@@ -0,0 +1,12 @@
+package rocketshell.vialactea.service;
+
+import org.springframework.stereotype.Service;
+
+import rocketshell.vialactea.base.BaseService;
+import rocketshell.vialactea.domain.RegistroDeColetaDeLeite;
+import rocketshell.vialactea.repository.RegistroDeColetaDeLeiteRepository;
+
+@Service
+public class RegistroDeColetaDeLeiteService extends BaseService<RegistroDeColetaDeLeite, RegistroDeColetaDeLeiteRepository> {
+
+}
