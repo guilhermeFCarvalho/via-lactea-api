f4216e9e2f1c87e698b77124a06c22dccf2db197
rodrigo <rodrigomarinelo2@gmail.com>
2022-11-09 21:11:44 -0300

    implementando metodo que testa se esta salvando o recibo de venda

--- src/test/java/rocketshell/vialactea/integration/ReciboDeVendaControllerIntTest.java
+++ src/test/java/rocketshell/vialactea/integration/ReciboDeVendaControllerIntTest.java
@@ -1,17 +1,23 @@
 package rocketshell.vialactea.integration;
 
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
 
 import lombok.SneakyThrows;
 import rocketshell.vialactea.domain.PessoaJuridica;
@@ -64,5 +70,49 @@ public class ReciboDeVendaControllerIntTest extends IntegrationTest {
 
     }
 	
+	
+	 @Test
+	 @SneakyThrows
+	 void shouldSaveReciboDeVenda() {
+		    BigDecimal bigdecimal = new BigDecimal(200);
+			LocalDate data = LocalDate.now();
+			PessoaJuridica pessoaJuridica = new PessoaJuridica();
+			Propriedade propriedade = new Propriedade();
+			
+			propriedade.setCar("1111");
+			propriedade.setTelefone("22222");
+			
+			pessoaJuridica.setCNPJ("22222");
+			pessoaJuridica.setInscricaoEstadual("222");
+			pessoaJuridica.setTelefone("222222");
+		
+	        ReciboDeVenda reciboDeVenda = new ReciboDeVenda();
+	        reciboDeVenda.setObservacoes("observação do registro de venda");
+	        reciboDeVenda.setPago(true);
+	        reciboDeVenda.setQuantidadeLeiteVendida(bigdecimal);
+	        reciboDeVenda.setDataDaVenda(data);
+	        reciboDeVenda.setPessoaJuridica(pessoaJuridica);
+	        reciboDeVenda.setPropriedade(propriedade);
+	        
+	        
+	        String jsonReciboDeVendaString = objectMapper.writeValueAsString(reciboDeVenda);
+	        
+	        MvcResult result = mockMvc.perform(post("/api/recibo-de-venda/")
+	                .contentType(MediaType.APPLICATION_JSON)
+	                .content(jsonReciboDeVendaString))
+	                .andExpect(status().is2xxSuccessful())
+	                .andReturn();
+	        
+	        ReciboDeVenda reciboDeVendaResponse = objectMapper.readValue(result.getResponse().getContentAsString(StandardCharsets.UTF_8), ReciboDeVenda.class);
+            
+	        assertNotNull(reciboDeVendaResponse.getId());
+	        
+	      
+	        assertTrue(reciboDeVendaResponse.getPago()== true);
+	        assertTrue(reciboDeVendaResponse.getPessoaJuridica().getCNPJ().equals("22222"));
+	        assertTrue(reciboDeVendaResponse.getObservacoes().equals("observação do registro de venda"));
+
+	 }
+
 
 }
