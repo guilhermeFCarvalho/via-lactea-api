78ae9d7a47b458f9e36d7d3f1614bbded6ee3c5f
willian.melo <willian.melo@elotech.com.br>
2022-11-15 02:09:05 -0300

    Criando endpoint para trazer recibos por propriedadeId de forma paginada

--- src/main/java/rocketshell/vialactea/controller/ReciboDeVendaController.java
+++ src/main/java/rocketshell/vialactea/controller/ReciboDeVendaController.java
@@ -1,6 +1,12 @@
 package rocketshell.vialactea.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +15,21 @@ import rocketshell.vialactea.domain.ReciboDeVenda;
 import rocketshell.vialactea.repository.ReciboDeVendaRepository;
 import rocketshell.vialactea.service.ReciboDeVendaService;
 
+import java.util.List;
+
 @Controller
 @RestController
 @RequestMapping("/api/recibo-de-venda")
-public class ReciboDeVendaController extends BaseController<ReciboDeVenda, ReciboDeVendaRepository, ReciboDeVendaService>{
+public class ReciboDeVendaController extends BaseController<ReciboDeVenda,
+                                                            ReciboDeVendaRepository,
+                                                            ReciboDeVendaService>{
+
+    @Autowired
+    private ReciboDeVendaService service;
+
+    @GetMapping("/propriedade/{id}")
+    public ResponseEntity<Page<ReciboDeVenda>> buscarRecibos(@PathVariable("id") Long id, Pageable pageable) {
+        return ResponseEntity.ok(service.listarRecibosDeUmaPropriedade(id,pageable));
+    }
+
 }
