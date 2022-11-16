441176f8677b5f5a87f8e45ddf51053090cf5df4
rodrigo <rodrigomarinelo2@gmail.com>
2022-11-16 20:54:07 -0300

    adicionando mais métodos de testes para testar a entidade propriedades

--- src/test/java/rocketshell/vialactea/integration/PropriedadeControllerIntTest.java
+++ src/test/java/rocketshell/vialactea/integration/PropriedadeControllerIntTest.java
@@ -1,18 +1,25 @@
 package rocketshell.vialactea.integration;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import utils.CustomPageImpl;       
+import com.fasterxml.jackson.core.type.TypeReference;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -124,7 +131,165 @@ public class PropriedadeControllerIntTest extends IntegrationTest{
 
 	  }
 
+	 @Test
+	 @SneakyThrows
+	 void shouldFindAllPageablePropriedade() {
+		 
+		 List<String> cars = Arrays.asList(
+				 "35005500200507","65005500200507","15005500200507",
+				 "25005500200507","23005500200507","34005500200507"
+				 );
+		 
+		 cars.forEach(car -> {
+			    Fazenda fazenda = new Fazenda();
+				Endereco endereco = new Endereco();
+				Propriedade propriedade = new Propriedade();
+				
+				endereco.setRua("estrada Brasil");
+				endereco.setBairro("vila rural");
+				endereco.setCep("87050-465");
+				endereco.setCidade("Marialva");
+				endereco.setEstado("Paraná");
+				endereco.setNumero("800");
+				
+				fazenda.setEndereco(endereco);
+				fazenda.setNomeDaFazenda("sitio 123");
+				
+				propriedade.setCar(car);
+				propriedade.setTelefone("77719898");
+				propriedade.setFazenda(fazenda);
+		
+			 propriedadeRepository.save(propriedade);
+		 });
+		 
+		 MvcResult result = mockMvc.perform(get("/api/propriedades?size=4&sort=car")
+	                .contentType(MediaType.APPLICATION_JSON))
+	                .andExpect(status().isOk()).andReturn();
+
+		 
+		 
+		 Page<Propriedade> pagePropriedade = objectMapper.readValue(result.getResponse().getContentAsString(StandardCharsets.UTF_8),
+				 new TypeReference<CustomPageImpl<Propriedade>>() {
+         });
+
+		 
+		 assertEquals(4, pagePropriedade.getSize());
+		 assertThat(pagePropriedade.getContent()).hasSize(4)
+           .extracting(Propriedade::getCar)
+           .containsExactlyInAnyOrder("15005500200507", "23005500200507","25005500200507","34005500200507");
+
+
+
+		
+	 }
 	 
+	 
+	 @Test
+	 @SneakyThrows
+	 void shouldFindByIdPropriedade() {
+	      
+		    Fazenda fazenda = new Fazenda();
+			Endereco endereco = new Endereco();
+			Propriedade propriedade = new Propriedade();
+			
+			endereco.setRua("estrada Brasil");
+			endereco.setBairro("vila rural");
+			endereco.setCep("87050-465");
+			endereco.setCidade("Marialva");
+			endereco.setEstado("Paraná");
+			endereco.setNumero("500");
+			
+			fazenda.setEndereco(endereco);
+			fazenda.setNomeDaFazenda("sitio 124");
+			
+			propriedade.setCar("35005500200507");
+			propriedade.setTelefone("77719898");
+			propriedade.setFazenda(fazenda);
+			
+	        propriedade = propriedadeRepository.save(propriedade);
+		 
+	        MvcResult result = mockMvc.perform(get("/api/propriedades/" + propriedade.getId().toString())
+	                .contentType(MediaType.APPLICATION_JSON))
+	                .andExpect(status().isOk()).andReturn();
+		  
+	        
+	        Propriedade propriedadeResponse = objectMapper.readValue(result.getResponse().getContentAsString(StandardCharsets.UTF_8), Propriedade.class);
+		  
+	        
+	        assertEquals(propriedadeResponse.getFazenda().getEndereco().getBairro(),"vila rural");
+	        assertEquals(propriedadeResponse.getFazenda().getEndereco().getRua(),"estrada Brasil");
+	        assertEquals(propriedadeResponse.getFazenda().getEndereco().getCep(),"87050-465");
+	        assertEquals(propriedadeResponse.getFazenda().getEndereco().getCidade(),"Marialva");
+	        assertEquals(propriedadeResponse.getFazenda().getEndereco().getEstado(),"Paraná");
+	        assertEquals(propriedadeResponse.getFazenda().getEndereco().getNumero(),"500");
+	        assertEquals(propriedadeResponse.getFazenda().getNomeDaFazenda(),"sitio 124");
+	        assertEquals(propriedadeResponse.getCar(),"35005500200507");
+	        assertEquals(propriedadeResponse.getTelefone(),"77719898");
+	 }
+	 
+	 
+	 @Test
+	 @SneakyThrows
+	 void shouldFindAllPropriedades() {
+		 
+		 List<String> cars = Arrays.asList(
+				 "35005500200507","65005500200507","15005500200507",
+				 "25005500200507","23005500200507","34005500200507"
+		 );
+		 
+		 cars.forEach(car -> {
+			    Fazenda fazenda = new Fazenda();
+				Endereco endereco = new Endereco();
+				Propriedade propriedade = new Propriedade();
+				
+				endereco.setRua("estrada Brasil");
+				endereco.setBairro("vila rural");
+				endereco.setCep("87050-465");
+				endereco.setCidade("Marialva");
+				endereco.setEstado("Paraná");
+				endereco.setNumero("800");
+				
+				fazenda.setEndereco(endereco);
+				fazenda.setNomeDaFazenda("sitio 123");
+				
+				propriedade.setCar(car);
+				propriedade.setTelefone("77719898");
+				propriedade.setFazenda(fazenda);
+		
+			    propriedadeRepository.save(propriedade);
+		 });
+		 
+		 
+		 MvcResult result = mockMvc.perform(get("/api/propriedades/list")
+	                .contentType(MediaType.APPLICATION_JSON))
+	                .andExpect(status().isOk()).andReturn();
+
+		
+		 
+		 List<Propriedade> propriedades = objectMapper.readValue(result.getResponse().getContentAsString(StandardCharsets.UTF_8),
+				 
+				 new TypeReference<List<Propriedade>>() {
+         }
+		 
+		);
+		 
+		 
+		 assertEquals(6, propriedades.size());
+		 assertEquals("35005500200507",propriedades.get(0).getCar());
+		 assertEquals("77719898",propriedades.get(0).getTelefone());
+		 assertEquals("25005500200507",propriedades.get(3).getCar());
+		 assertEquals("Marialva",propriedades.get(3).getFazenda().getEndereco().getCidade());
+		 assertEquals("34005500200507",propriedades.get(5).getCar());
+		
+
+		
+	 }
+	 
+	 
+	 
+
+
+
 
 
 }
